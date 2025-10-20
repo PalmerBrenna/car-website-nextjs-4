4266109e2edc7960ae15efd42088e05aeea94a84
Palmer <Palmer_Brenna@proton.me>
2025-10-20 16:47:50 +0100

    install

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">🚗 Car Website Works!</h1>
+    </main>
+  );
+}
