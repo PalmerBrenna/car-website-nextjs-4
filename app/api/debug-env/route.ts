32b841fe36ff8bdd2343c129cf984d3b80289b2d
Palmer <Palmer_Brenna@proton.me>
2025-11-21 15:34:28 +0000

    Update route.ts

--- app/api/debug-env/route.ts
+++ app/api/debug-env/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+/*import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.json({
@@ -8,3 +8,4 @@ export async function GET() {
     FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET || null,
   });
 }
+*/
